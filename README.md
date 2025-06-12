97d10e6e068e3f323e4184a271fb7c98445666ec
Julia Taga <148263467+juliataga@users.noreply.github.com>
2025-06-13 00:22:32 +0530

    upload initial
    just a basic python project

---
+++ README.md
@@ -0,0 +1,21 @@
+# Number Guessing Game
+
+A simple Python game where the player tries to guess a randomly chosen number within a specified range.
+
+## How it Works
+
+- The player inputs a lower and upper bound.
+- The game randomly selects a number in that range.
+- The player has 7 chances to guess the correct number.
+- After each guess, the game gives a hint:
+  - Too high
+  - Too low
+  - Correct
+- If all attempts are used without guessing correctly, the correct number is revealed.
+
+## How to Run
+
+Make sure Python is installed, then run the script:
+
+```bash
+python guess_game.py
---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+
+print("Welcome to my no guessing game!")
+
+lower_range = int(input("enter lower range: "))
+upper_range = int(input("enter upper range: "))
+
+target = random.randint(lower_range,upper_range)
+print(target)
+
+total_guess = 7
+guess_counter = 0
+
+while total_guess > guess_counter:
+    your_guess = int(input("guess a number: "))
+    
+    if your_guess == target:
+        print("Great! you guessd it right")
+        break
+
+    elif your_guess >= target:
+        print("Ops! too highh! try again :)")
+
+    elif your_guess <= target:
+        print("sry too low, try again")
+    
+    guess_counter += 1
+
+if guess_counter == total_guess and your_guess != target:
+    print(f' You Lost:( {target} was the number')
+
